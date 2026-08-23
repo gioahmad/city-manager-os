2a89acf4b2fe3a759d01369b19a5f9d1d3049cce
gioahmad <99091309+gioahmad@users.noreply.github.com>
2026-08-22 23:07:29 -0400

    Mount dashboard static assets

--- dashboard/app.py
+++ dashboard/app.py
@@ -5,9 +5,11 @@ import psycopg
 from psycopg.rows import dict_row
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI(title="City Manager OS Dashboard", version="0.1")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
